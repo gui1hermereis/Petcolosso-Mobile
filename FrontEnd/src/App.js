c69ca66ce36c9dbc77c23297898dad9c52bc1809
Guilherme Reis <sjcgui.2004@gmail.com>
2024-08-25 22:21:47 -0300

    First commit

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,12 +4,13 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './components/Login';
+import Inicio from './components/Inicio'
+import Servico from './components/Servico';
+import Carrinho from './components/Carrinho';
+import Finalizarcompra from './components/Finalizarcompra';
+
 global.totalPago
-import Login from './src/components/Login';
-import Inicio from './src/components/Inicio'
-import Servico from './src/components/Servico';
-import Carrinho from './src/components/Carrinho';
-import Finalizarcompra from './src/components/Finalizarcompra';
 
 const Stack = createStackNavigator();
 
